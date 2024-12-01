00b6f3d0a51d936ab4d48a05db5f29caf00174fd
Brad Davies <brad.davies@withwise.com>
2024-12-01 17:33:29 +0000

    Change naming

--- src/day01.ts
+++ src/day01.ts
@@ -1,17 +1,23 @@
 export const part1 = (input: string): number => {
-  const parsed = input.trim().split('\n').map(l => l.split(/\s+/).map(Number))
+  const parsed = input
+    .trim()
+    .split('\n')
+    .map(l => l.split(/\s+/).map(Number))
   const a = parsed.map(a => a[0]).sort((a, b) => a - b)
   const b = parsed.map(a => a[1]).sort((a, b) => a - b)
   return a.reduce((acc, v, i) => acc + Math.abs(v - b[i]), 0)
 }
 
 export const part2 = (input: string): number => {
-  const parsed = input.trim().split('\n').map(l => l.split(/\s+/).map(Number))
+  const parsed = input
+    .trim()
+    .split('\n')
+    .map(l => l.split(/\s+/).map(Number))
   const a = parsed.map(a => a[0]).sort((a, b) => a - b)
   const b = parsed.map(a => a[1]).sort((a, b) => a - b)
 
   return a.reduce((acc, v, i) => {
     const occurences = b.filter(x => x === v).length
-    return acc + (v*occurences)
+    return acc + v * occurences
   }, 0)
 }
--- test/day01.test.ts
+++ test/day01.test.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'bun:test';
-import * as day1 from '../src/day1';
+import { describe, expect, it } from 'bun:test'
 import fs from 'node:fs/promises'
 import path from 'node:path'
+import * as day1 from '../src/day01'
 
 describe('d1:p1', () => {
   it('should solve the example input', () => {
@@ -17,7 +17,10 @@ describe('d1:p1', () => {
   })
 
   it('should solve the puzzle input', async () => {
-    const input = await fs.readFile(path.resolve(__dirname, '../input/day1.txt'), 'utf-8')
+    const input = await fs.readFile(
+      path.resolve(__dirname, '../input/day1.txt'),
+      'utf-8',
+    )
     expect(day1.part1(input)).toEqual(1222801)
   })
 })
@@ -36,7 +39,10 @@ describe('d1:p2', () => {
   })
 
   it('should solve the puzzle input', async () => {
-    const input = await fs.readFile(path.resolve(__dirname, '../input/day1.txt'), 'utf-8')
+    const input = await fs.readFile(
+      path.resolve(__dirname, '../input/day1.txt'),
+      'utf-8',
+    )
     expect(day1.part2(input)).toEqual(22545250)
   })
 })
