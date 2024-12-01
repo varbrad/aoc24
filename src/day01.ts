9b6cf1a7dc2514bc9d76caad15228c17f1d67a36
Brad Davies <brad.davies@withwise.com>
2024-12-01 17:46:53 +0000

    [day01] Tidy

--- src/day01.ts
+++ src/day01.ts
@@ -1,21 +1,21 @@
-export const part1 = (input: string): number => {
+const parse = (input: string): [number[], number[]] => {
   const parsed = input
     .trim()
     .split('\n')
     .map(l => l.split(/\s+/).map(Number))
-  const a = parsed.map(a => a[0]).sort((a, b) => a - b)
-  const b = parsed.map(a => a[1]).sort((a, b) => a - b)
+  return [
+    parsed.map(a => a[0]).sort((a, b) => a - b),
+    parsed.map(a => a[1]).sort((a, b) => a - b),
+  ]
+}
+
+export const part1 = (input: string): number => {
+  const [a, b] = parse(input)
   return a.reduce((acc, v, i) => acc + Math.abs(v - b[i]), 0)
 }
 
 export const part2 = (input: string): number => {
-  const parsed = input
-    .trim()
-    .split('\n')
-    .map(l => l.split(/\s+/).map(Number))
-  const a = parsed.map(a => a[0]).sort((a, b) => a - b)
-  const b = parsed.map(a => a[1]).sort((a, b) => a - b)
-
+  const [a, b] = parse(input)
   return a.reduce((acc, v, i) => {
     const occurences = b.filter(x => x === v).length
     return acc + v * occurences
