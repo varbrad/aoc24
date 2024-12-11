5944f2d4121c779cd57f86228ba18659c8785699
Brad Davies <brad.davies@withwise.com>
2024-12-11 17:07:28 +0000

    [day-07] refactor

--- src/day07.ts
+++ src/day07.ts
@@ -1,75 +1,44 @@
-type Row = { target: number; values: number[] }
-
-const parse = (input: string): Row[] =>
+const parse = (input: string): { target: number; values: number[] }[] =>
   input
     .trim()
     .split('\n')
-    .map(r => {
-      const [target, values] = r.split(': ')
+    .map(row => {
+      const [a, b] = row.split(': ')
       return {
-        target: Number(target),
-        values: values.trim().split(' ').map(Number),
+        target: Number(a.trim()),
+        values: b.trim().split(' ').map(Number),
       }
     })
 
-type Ops = '+' | '*' | '|'
+export const canBeSolved = (
+  target: number,
+  values: number[],
+  withConcat?: boolean,
+): boolean => {
+  // If we have only 1 value left, does the target equal it?
+  if (values.length === 1) return target === values[0]
 
-const calc = (a: number, b: number, op: Ops): number => {
-  switch (op) {
-    case '+':
-      return a + b
-    case '*':
-      return a * b
-    case '|':
-      return Number(`${a}${b}`)
-  }
+  const lastValue = values[values.length - 1]
+
+  const next = []
+  const div = target / lastValue
+  if (Number.isInteger(div)) next.push(div)
+
+  if (withConcat && String(target).endsWith(String(lastValue)))
+    next.push(Number(String(target).slice(0, -String(lastValue).length)))
+
+  const sub = target - lastValue
+  if (sub > 0) next.push(sub)
+
+  return next.some(n => canBeSolved(n, values.slice(0, -1), withConcat))
 }
 
-const canSolveInstance = (row: Row, ops: Ops[]): boolean => {
-  let total = row.values[0]
-  for (let i = 1; i < row.values.length; i++) {
-    total = calc(total, row.values[i], ops[i - 1])
-  }
-  return total === row.target
-}
-
-const permuteCache: Map<string, Ops[][]> = new Map()
-const generatePermutations = (length: number, withConcat: boolean): Ops[][] => {
-  const cacheValue = permuteCache.get(`${length}-${withConcat}`)
-  if (cacheValue !== undefined) return cacheValue
-
-  const result: Ops[][] = []
-
-  const generate = (current: Ops[]): void => {
-    // Base case: If the current string reaches the desired length, add it to the result
-    if (current.length === length) {
-      result.push(current)
-      return
-    }
-
-    generate([...current, '*'])
-    generate([...current, '+'])
-    if (withConcat) generate([...current, '|'])
-  }
-
-  generate([])
-  permuteCache.set(`${length}-${withConcat}`, result)
-  return result
-}
-
-const canSolve = (row: Row, withConcat: boolean): boolean =>
-  generatePermutations(row.values.length - 1, withConcat).some(op =>
-    canSolveInstance(row, op),
-  )
-
-export const part1 = (input: string): number =>
-  parse(input).reduce(
-    (acc, row) => acc + (canSolve(row, false) ? row.target : 0),
+const solve = (rows: ReturnType<typeof parse>, withConcat?: boolean) =>
+  rows.reduce(
+    (acc, { target, values }) =>
+      acc + (canBeSolved(target, values, withConcat) ? target : 0),
     0,
   )
 
-export const part2 = (input: string): number =>
-  parse(input).reduce(
-    (acc, row) => acc + (canSolve(row, true) ? row.target : 0),
-    0,
-  )
+export const part1 = (input: string): number => solve(parse(input))
+export const part2 = (input: string): number => solve(parse(input), true)
